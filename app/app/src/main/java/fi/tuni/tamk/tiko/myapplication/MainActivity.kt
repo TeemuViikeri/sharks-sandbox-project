9f4f164f23183aa9767f5fecfb00d96abb1c67b1
Teemu Viikeri <teemu.viikeri@tuni.fi>
2021-05-11 11:47:19 +0300

    Add sorting buttons to roster.

--- app/app/src/main/java/fi/tuni/tamk/tiko/myapplication/MainActivity.kt
+++ app/app/src/main/java/fi/tuni/tamk/tiko/myapplication/MainActivity.kt
@@ -3,12 +3,17 @@ package fi.tuni.tamk.tiko.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.fasterxml.jackson.databind.ObjectMapper
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -66,28 +71,32 @@ class MainActivity : AppCompatActivity() {
     val contentEndpoint: String = "game/$gameID/content"
 
     /**
-     * Content endpoint expand that returns only editorial preview content,
-     * especially projected lineups.
+     * Content endpoint expand that returns only editorial preview content.
+     * Includes projected lineups via generated token objects.
      */
     val previewContentExpand: String = "schedule.game.content.editorial.preview"
 
     /**
-     *
+     * Title text view for the activity. Displays an NHL team's name.
      */
     private lateinit var title: TextView
 
     /**
-     *
+     * RecyclerView for displaying team's roster.
      */
     private lateinit var rvRoster: RecyclerView
 
     /**
-     *
+     * Adapter for RecyclerView that displays the roster.
      */
     private lateinit var rosterAdapter: RosterAdapter
 
     /**
+     * Lifecycle method. Makes the initial API calls to the NHL API to display
+     * team roster info when application has been loaded after start.
      *
+     * @param savedInstanceState Bundle state data that is used to create
+     * the initial activity.
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -98,26 +107,45 @@ class MainActivity : AppCompatActivity() {
         val apiURL = "$baseURL/$teamEndpoint?expand=" +
                 "$rosterExpand,$scheduleNextExpand,$schedulePreviousExpand"
 
-        Log.d("url", apiURL)
-
         val http = HttpConnection()
         http.fetchAsync(apiURL, this) {
+            // Get Team object from JSON
             val team = convertJsonToTeamObject(it)
-            Log.d("url", team.toString())
+            // Set title of the activity to be the name of the team
             title.text = team.name
 
-            val roster = sortPlayersByJerseyNumber(team.roster.roster)
-            
+            // Setup roster, Adapter and RecyclerView
+            val roster = team.roster.roster
             rosterAdapter = RosterAdapter(roster)
+            rosterAdapter.sortPlayersByJerseyNumber()
             rvRoster.adapter = rosterAdapter
             rvRoster.layoutManager = LinearLayoutManager(this)
 
+            // Setup roster buttons and their onClickListeners
+            val btnSortByJerseyNumber: Button = findViewById(R.id.btnSortByJerseyNumber)
+            val btnSortByName: Button = findViewById(R.id.btnSortByName)
+            val btnSortByPosition: Button = findViewById(R.id.btnSortByPosition)
+
+            btnSortByJerseyNumber.setOnClickListener {
+                rosterAdapter.sortPlayersByJerseyNumber()
+            }
+
+            btnSortByName.setOnClickListener {
+                rosterAdapter.sortPlayersByName()
+            }
+
+            btnSortByPosition.setOnClickListener {
+                rosterAdapter.sortPlayersByPosition()
+            }
+
+            // Setup roster list item dividers
             val drawable = ContextCompat.getDrawable(this, R.drawable.roster_divider)
             val divider = DividerItemDecoration(
                 rvRoster.context, (rvRoster.layoutManager as LinearLayoutManager).orientation)
             drawable?.let { dr -> divider.setDrawable(dr) }
             rvRoster.addItemDecoration(divider)
 
+            // Setup next match information
             val awayTeam: TextView = findViewById(R.id.tvAway)
             val homeTeam: TextView = findViewById(R.id.tvHome)
             val awayTeamRecord: TextView = findViewById(R.id.tvAwayRecord)
@@ -139,21 +167,61 @@ class MainActivity : AppCompatActivity() {
             val homeOts = homeTeamSeasonRecords.ot.toString()
             homeTeamRecord.text = "$homeWins-$homeLosses-$homeOts"
 
+            val matchDate = team.nextGameSchedule.dates[0].date
+            val formattedDate = matchDate.toDate("yyyy-MM-dd").let {
+                    it1 -> it1?.formatTo("dd MMM yyyy")
+            }
+            Log.d("matchdate", formattedDate.toString())
             val matchVenue = team.nextGameSchedule.dates[0].games[0].venue.name
-            nextMatchVenue.text = "@$matchVenue"
+            nextMatchVenue.text = "${formattedDate.toString()} @ $matchVenue"
         }
     }
 
     /**
+     * Converts JSON data of a team from NHL API to Jackson mapped Team object.
      *
+     * @param json Returned JSON data from the NHL API's team endpoint.
+     * @return Converted Team object.
      */
     private fun convertJsonToTeamObject(json: String): Team {
         val mp = ObjectMapper()
         val teamData: TeamData = mp.readValue(json, TeamData::class.java)
         return teamData.teams[0]
     }
-    
-    private fun sortPlayersByJerseyNumber(players: List<Player>): List<Player> {
-        return players.sortedBy { it.jerseyNumber.toInt() }
+
+    /**
+     * Converts a date string into a parsed Date object.
+     *
+     * You can use this function to represent match dates in user's own
+     * date format and time zone.
+     *
+     * @param dateFormat Date format used in the parsed date.
+     * @param timeZone The time zone in which the parsed date should be in.
+     * @return Parsed Date object.
+     */
+    private fun String.toDate(
+        dateFormat: String = "yyyy-MM-dd HH:mm:ss",
+        timeZone: TimeZone = TimeZone.getTimeZone("UTC")
+    ): java.util.Date? {
+        val parser = SimpleDateFormat(dateFormat, Locale.getDefault())
+        parser.timeZone = timeZone
+        return parser.parse(this)
+    }
+
+    /**
+     * Formats a Date object into a string representation with preferred
+     * date format and time zone.
+     *
+     * @param dateFormat Preferred date format of the formatted date string.
+     * @param timeZone Time zone used in the formatting.
+     * @return String representation of a Date object.
+     */
+    private fun java.util.Date.formatTo(
+        dateFormat: String,
+        timeZone: TimeZone = TimeZone.getDefault()
+    ): String {
+        val formatter = SimpleDateFormat(dateFormat, Locale.getDefault())
+        formatter.timeZone = timeZone
+        return formatter.format(this)
     }
 }
--- app/app/src/main/java/fi/tuni/tamk/tiko/myapplication/RosterAdapter.kt
+++ app/app/src/main/java/fi/tuni/tamk/tiko/myapplication/RosterAdapter.kt
@@ -7,15 +7,21 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class RosterAdapter(
-    private val players: List<Player>
+    private var players: List<Player>
 ) : RecyclerView.Adapter<RosterAdapter.RosterViewHolder>() {
 
+    /**
+     *
+     */
     inner class RosterViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val tvPlayerNumber: TextView = view.findViewById(R.id.tvPlayerNumber)
         val tvPlayerName: TextView = view.findViewById(R.id.tvPlayerName)
         val tvPlayerPosition: TextView = view.findViewById(R.id.tvPlayerPosition)
     }
 
+    /**
+     *
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RosterViewHolder {
         return RosterViewHolder(
             LayoutInflater.from(parent.context).inflate(
@@ -26,6 +32,9 @@ class RosterAdapter(
         )
     }
 
+    /**
+     *
+     */
     override fun onBindViewHolder(holder: RosterViewHolder, position: Int) {
         val player: Player = players[position]
         holder.tvPlayerNumber.text = player.jerseyNumber
@@ -33,7 +42,41 @@ class RosterAdapter(
         holder.tvPlayerPosition.text = player.position.abbreviation
     }
 
+    /**
+     *
+     */
     override fun getItemCount(): Int {
         return players.size
+    }
+
+    /**
+     * Sorts a list of Player objects numerically by their jersey number property.
+     *
+     * @return Sorted list of Players.
+     */
+    fun sortPlayersByJerseyNumber() {
+        this.players = players.sortedBy { it.jerseyNumber.toInt() }
+        notifyDataSetChanged()
+    }
+
+    /**
+     * Sorts a list of Player objects alphabetically by their name property.
+     *
+     * @return Sorted list of Players.
+     */
+    fun sortPlayersByName() {
+        this.players = players.sortedWith(
+            compareBy(String.CASE_INSENSITIVE_ORDER, { it.person.fullName }))
+        notifyDataSetChanged()
+    }
+
+    /**
+     * Sorts a list of Player objects alphabetically by their position property.
+     *
+     * @return Sorted list of Players.
+     */
+    fun sortPlayersByPosition(){
+        this.players = players.sortedBy { it.position.toString() }
+        notifyDataSetChanged()
     }
 }
